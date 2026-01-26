e4d6aba33b872ec12853bc98350e62f4dfed2171
keroro7729 <keroro06108@gmail.com>
2026-01-26 15:45:52 +0900

    feat: 3type Exception 적용 (요청, 내부, 외부)
    Signed-off-by: keroro7729 <keroro06108@gmail.com>

---
+++ src/main/java/orchestrator/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package orchestrator.exceptions;
+
+public class BadRequestException extends AppException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/orchestrator/exceptions/InternalServerException.java
@@ -0,0 +1,8 @@
+package orchestrator.exceptions;
+
+public class InternalServerException extends AppException {
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
